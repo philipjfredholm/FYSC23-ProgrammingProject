9ee1b8fca15cbb392940af4b1e8950a2a0d077fc
Philip J. Fredholm <philip.j.fredholm@gmail.com>
2023-02-22 11:28:04 +0100

    .

--- README.md
+++ README.md
@@ -1,1 +1,115 @@
 # FYSC23-ProgrammingProject
+
+
+
+
+
+
+    /*
+    Now we want to find the initial values c_m(0) by minimising the energy.
+    In general, this seems like a hard problem as we need to both minimise
+    \sum E_n |c_n|^2 and keep the normalisation constraint on \sum |c_n|^2.
+    However, that we have negative eigenvalues simplifies this greatly as the 
+    minimum is then obviously when we set all other c_m to 0 and the one with the 
+    most negative eigenvalue to magnitude 1 (or just 1 since we have global gauge invariance).
+    */
+
+    /*
+    Since the energy-eigenstates basis \varphi is already given in the basis \phi,
+    all we need to do is normalise the eigenvector correspond to the minimum (negative)
+    eigenenergy to find c_m(0)
+    */
+
+    /*
+    We now want c_m^\lambda. Since the energy-eigenbasis is already given in the desired basis,
+    <\phi_m|\varphi_\lambda> is just the n:th row of the identity matrix mutliplied with 
+    the \lambda:th eigen(column)vector. (In other words we just set all elements except m to 0).
+
+    We need to keep in mind that since epsilon is not small (\pm 2) compared to V which is -1,
+    this does not just peturb the energy-eigenstate. We get the initial c_m() from the
+    matrix in equation (28) in the manual, but we need to project it onto the new energy-eigen basis once
+    the heaviside theta-function kicks in. This is what equation (11) in the manual does. To do this, we
+    use that both of these are given in the \phi basis, which is orthogonal. Hence,
+    all we need to do is take the dot product.
+    */
+VectorXd makeReal(VectorXcd complexVector) {
+    /* 
+    The Eigen software gives back a vector
+    of possibly complex eigenvalues. This just 
+    makes that vector real and double checks so that
+    they are not complex 
+    */
+
+    int length = complexVector.size();
+    VectorXd realVector(length);
+
+    for (int n = 0; n < length; n++) {
+        if (complexVector(n).imag() != 0) {
+            std::cout << "Warning: non-real eigenvalues detected!" << std::endl;
+        }
+
+        realVector(n) =  complexVector(n).real();
+    }
+
+    return realVector;
+
+}
+
+
+
+void makePlot(EigenSolver<MatrixXd> hamiltonian, VectorXd initialValues,
+            double timeInterval, double timeStepLength, int argc, char** argv) {
+
+    int length = initialValues.size();
+    const char* title = "Values of the coefficients at #t = ";
+    std::string stringTitle = "Values of the coefficients at #t = ";
+
+    //ROOT handles removing things from the heap for us
+    TRint* application = new TRint("application", &argc, argv);
+    TCanvas* canvas = new TCanvas("canvas", "Simulation Result", 0, 0, 800, 600);
+    TH1D* hist = new TH1D("hist", title, length, 1, length);
+    hist->GetXaxis()->SetTitle("Well Number");
+    hist->GetYaxis()->SetTitle("#rho = |c_{n}|^{2}");
+    hist->GetXaxis()->CenterTitle(true);
+    hist->GetYaxis()->CenterTitle(true);
+    hist->GetXaxis()->SetTitleSize(0.04);
+    hist->GetYaxis()->SetTitleSize(0.045);
+    hist->SetMaximum(0.5);
+    hist->SetStats(0);
+    hist->SetFillColor(kBlue-5);
+
+    canvas->Show();
+
+    double time = 0;
+    int million = 1000000;
+    std::complex<double> coefficient;
+    while (time <= timeInterval) {
+        hist->Reset();
+        for (int k = 1; k <= length; k++) { //Start at 1 to not go into underflow bin
+            coefficient = coefficients(k-1, time, hamiltonian, initialValues);
+            double magnitude = pow(std::abs(coefficient),2);
+            //magnitude *= 1000; //Histogram only acccepts integer entries.
+            //magnitude = std::ceil(magnitude);
+            hist->SetBinContent(k, magnitude);
+
+        }
+
+
+        //std::string timeString = stringTitle + std::to_string(time);
+        //const char* newTitle = timeString.c_str();
+        //hist->SetTitle(newTitle);
+
+        hist->Draw();
+        canvas->Update();
+
+        usleep(timeStepLength*million); //usleep() works in microseconds
+        time += timeStepLength;
+    }
+
+
+    
+    
+    application->Run();
+
+
+}
--- main.cpp
+++ main.cpp
@@ -3,57 +3,45 @@
 #include <vector>
 #include <cmath>
 #include <complex>
+#include <algorithm>
 #include <Eigen/Dense>
 #include <Eigen/Eigenvalues>
+#include<unistd.h> //Replace with #include <windows.h> if not running Linux.
 
 using namespace Eigen;
 
+//ROOT headers
+#include <TRint.h>
+#include <TCanvas.h>
+#include <TStyle.h>
 #include <TH1D.h>
 
+MatrixXd makeReal(MatrixXcd complexMatrix) {
+    int rows = complexMatrix.rows();
+    int columns = complexMatrix.cols();
+    MatrixXd realMatrix(rows, columns);
 
-/*
-double coefficient(int N, double startValue, int nStates, double orbitalEnergy, double time) {
-    std::complex<double> i {0, 1}; //Defines the imaginary unit
-    double reducedPlanck = 1; //1.054571817*std::pow(10, -34);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            if (complexMatrix(i,j).imag() != 0) {
+                std::cout << "Warning: non-real values detected!" << std::endl;
+            }
 
-    std::complex<double> value  {0, 0};         //Value to be returned
-    std::complex<double> exponential {0, 0}; //Placeholder value
-    for (int k = 0; int k > nStates; k++) {
-        exponential = std::exp(-i*orbitalEnergy*time/reducedPlanck);
-        
+            realMatrix(i, j) = complexMatrix(i, j).real();
 
-
+        }
     }
+
+    return realMatrix;
+
 }
-*/
-
-
-std::complex<double> coefficient(int N, ) {
-    
-}
-
-
-VectorXd projectCoefficients(VectorXd state);
-    /* Projects the coefficients of the energy eigenstates to the
-    desired basis. */
-
-    //Normalisation
-    VectorXd temp = state; //Necessary according to the documentation
-    double length = state.squaredNorm();
-    state = temp/length; 
-
-    //No need to project this onto the basis vectors as they are
-    //already just [1,0,...,0] , [0,1,0,...0] etc.
-
-    return state;
-
 
 MatrixXd hamiltonianConstructor(int size, double potential) {
-    MatrixXd matrix(size, size); //Defines the matrix
-    
+    //Constructs the matrix from equation (28) in the manual
+    MatrixXd matrix(size, size); 
+
     for (int n = 0; n < size; n++) {
         for (int m = 0; m < n; m++) {
-
             if (n == m+1) {
                 matrix(n,m) = potential;
                 matrix(m, n) = potential;
@@ -62,69 +50,135 @@ MatrixXd hamiltonianConstructor(int size, double potential) {
                 matrix(n, m) = 0;
                 matrix(m, n) = 0;
             }
-
-
-
         }
     }
-
+    
     return matrix;
 }
 
+std::complex<double> coefficients(int number, double time, 
+                                EigenSolver<MatrixXd> energyBasis, VectorXd initialValues) {
+    double hbar = 1;
+    std::complex<double> i {0, 1}; //Imaginary unit
+    std::complex<double> coefficient = {0, 0}; //Value to be returned
+    int problemSize = initialValues.size();
 
+    //Eigenenergies
+    VectorXd eigenEnergies = makeReal(energyBasis.eigenvalues());
 
-double energy(VectorXcd energies, VectorXd coefficients) {
-    double totalEnergy = 0;
-    for (int n = 0; n < energies.size(); n++) {
-        totalEnergy += real(energies[n]) * abs(coefficients[n]);
-        std::cout << real(energies[n]) << std::endl;
+    //Gets the basis vectors and normalises them
+    MatrixXd basis = makeReal(energyBasis.eigenvectors());
+    for (int n = 0; n < problemSize; n++) {
+        basis.col(n).normalize();
+    }
+
+    //Calculates the double sum
+    double internalSum = 0;
+    double projection = 0;
+    double secondProjection = 0;
+    for (int lambdaNum = 0; lambdaNum < problemSize; lambdaNum++) {
+        internalSum = 0;
+        for (int m = 0; m < problemSize; m++) {
+            projection = basis.col(lambdaNum)(m);   //c_m^lambdaNum  (m:th value in the lambda:th eigenvector)
+            internalSum += projection * initialValues(m) ;      //c_m^lambdaNum * c_m(0)
+
+        }
+
+        std::complex<double> exponential = std::exp(-i* eigenEnergies(lambdaNum)*time/hbar);
+        secondProjection = basis.col(lambdaNum)(number);
+
+        coefficient += exponential * secondProjection * internalSum;
 
     }
 
-    return totalEnergy;
+    return coefficient;
+
 }
 
 
 
-int main() {
 
-    //Parameters
+
+int main(int argc, char** argv) {
+    //Parameters for the simulation
     const int length = 6;
     const double potential = -1;
-    //const double timeInterval = 20;
+    const double timeInterval = 20;
+    const double timeStepLength = 0.01;
     const double perturbation = 2;
 
-    //Constructs the Hamiltonians and finds the eigenvalues (tantamount to diagonalising them)
-    MatrixXd rawHamiltonian = hamiltonianConstructor(length, potential);
-    MatrixXd plusHamiltonian = rawHamiltonian;
+
+
+    //Constructs the Hamiltonians.
+    MatrixXd rawHamiltonian = hamiltonianConstructor(length, potential);     //Equation (28) in the manual
+    MatrixXd plusHamiltonian = rawHamiltonian; 
     MatrixXd minusHamiltonian = rawHamiltonian;
-    plusHamiltonian(0,0) += perturbation;
+    plusHamiltonian(0,0) += perturbation;                                    //Equation (29) in the manual
     minusHamiltonian(0,0) -= perturbation;
 
-    EigenSolver< MatrixXd> raw(rawHamiltonian);
+    //Introduces necessary things to calculate eigenvectors and eigenvalues.
+    EigenSolver<MatrixXd> raw(rawHamiltonian);
     EigenSolver<MatrixXd> plus(plusHamiltonian);
     EigenSolver<MatrixXd> minus(minusHamiltonian);
 
-    VectorXcd rawEigenVals = rawHamiltonian.eigenvalues(); //Gives an error message if we do not accept complex values (Xcd instead of Xd)
-    VectorXcd plusEigenVals = plusHamiltonian.eigenvalues();
-    VectorXcd minusEigenVals = minusHamiltonian.eigenvalues();
+    //Finds the ground state values c_m(0) before the heavi-side function kicks in.
+    VectorXd rawEigenVals = makeReal(rawHamiltonian.eigenvalues());
+    auto minValue = std::min_element(rawEigenVals.begin(), rawEigenVals.end());
+    int minIndex = std::distance(rawEigenVals.begin(), minValue); 
 
-    //MatrixXcd rawEigenVecs = raw.eigenvectors();
-    //std::cout << rawEigenVecs.col(0) << std::endl;
+    VectorXd initialValuesTemp = makeReal(raw.eigenvectors().col(minIndex)); //I am aware of /= notation, but the documentation says to do it this way.
+    double norm = initialValuesTemp.squaredNorm();
+    VectorXd initialValues = initialValuesTemp/norm; //This gives the values c_m(0) in the \phi_n basis.
+
+    std::complex<double> test = coefficients(3, 0.5, plus, initialValues);
+    std::cout << test << "and " << initialValues(3) << std::endl;
+
+    //makePlot(plus, initialValues, timeInterval, timeStepLength, argc, argv);
+    TRint* application = new TRint("application", &argc, argv);
+    TCanvas* canvas = new TCanvas("canvas", "Simulation Result", 0, 0, 800, 600);
+    TH1D* hist = new TH1D("hist", "a", length, 1, length);
+    hist->GetXaxis()->SetTitle("Well Number");
+    hist->GetYaxis()->SetTitle("#rho = |c_{n}|^{2}");
+    hist->GetXaxis()->CenterTitle(true);
+    hist->GetYaxis()->CenterTitle(true);
+    hist->GetXaxis()->SetTitleSize(0.04);
+    hist->GetYaxis()->SetTitleSize(0.045);
+    hist->SetMaximum(0.5);
+    hist->SetStats(0);
+    hist->SetFillColor(kBlue-5);
+
+    canvas->Show();
+
+    double time = 0;
+    int million = 1000000;
+    std::complex<double> coefficient;
+    while (time <= timeInterval) {
+        hist->Reset();
+        for (int k = 1; k <= length; k++) { //Start at 1 to not go into underflow bin
+            coefficient = coefficients(k-1, time, plus, initialValues);
+            double magnitude = pow(std::abs(coefficient),2);
+            //magnitude *= 1000; //Histogram only acccepts integer entries.
+            //magnitude = std::ceil(magnitude);
+            hist->SetBinContent(k, magnitude);
+
+        }
 
 
+        //std::string timeString = stringTitle + std::to_string(time);
+        //const char* newTitle = timeString.c_str();
+        //hist->SetTitle(newTitle);
 
-    std::cout << raw.eigenvectors().col(0) << std::endl;
-    //std::cout << plusEigenVals << std::endl;
-    //std::cout << minusEigenVals << std::endl;
+        hist->Draw();
+        canvas->Update();
+
+        usleep(timeStepLength*million); //usleep() works in microseconds
+        time += timeStepLength;
+    }
 
 
-    //VectorXd initialCoefficients {{1, 2, 1, 9, 4, 5}};
-    //std::cout << initialCoefficients << std::endl;
-    //std::cout << energy(rawEigenVals, initialCoefficients) << std::endl;
     
-
-
+    
+    application->Run();
 
 
 }
